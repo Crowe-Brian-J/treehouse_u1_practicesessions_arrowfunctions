33ed24d4d1b804cfef1ba6702351ed1dd5a096dd
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-21 01:42:24 -0400

    convert.js update

--- convert.js
+++ convert.js
@@ -1,8 +1,6 @@
 const addToTen = (num) => 10 + num
 
-const divideUs = (num1, num2) => {
-  return num1 / num2
-}
+const divideUs = (num1, num2) => num1 / num2
 
 const printMyName = (myName = 'Ashley') => {
   console.log(myName)
